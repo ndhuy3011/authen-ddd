8b9859519b59ddbaf50b1c58d2de2e4b3615a81b
duchuy <ndhuy30111@gmail.com>
2025-01-13 18:40:15 +0700

    Refactor security configuration, add gRPC client configuration, and implement UserGrpc service for authentication

--- src/main/java/com/ndhuy/authen/SecurityConfiguration.java
+++ src/main/java/com/ndhuy/authen/SecurityConfiguration.java
@@ -2,24 +2,24 @@ package com.ndhuy.authen;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.JwtDecoders;
-import org.springframework.security.web.SecurityFilterChain;
+// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+// import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+// import org.springframework.security.oauth2.jwt.JwtDecoder;
+// import org.springframework.security.oauth2.jwt.JwtDecoders;
+// import org.springframework.security.web.SecurityFilterChain;
 
 import lombok.extern.slf4j.Slf4j;
 
-@Configuration
-@EnableWebSecurity
-@Slf4j
+// @Configuration
+// @EnableWebSecurity
+// @Slf4j
 public class SecurityConfiguration {
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
    
-        return http.build();
-    }
-    @Bean
-public JwtDecoder jwtDecoder() {
-    return JwtDecoders.fromIssuerLocation("https://idp.example.org");
-}
+//         return http.build();
+//     }
+//     @Bean
+// public JwtDecoder jwtDecoder() {
+//     return JwtDecoders.fromIssuerLocation("https://idp.example.org");
+// }
 }
---
+++ src/main/java/com/ndhuy/authen/users/infrastructure/UserGrpc.java
@@ -0,0 +1,27 @@
+package com.ndhuy.authen.users.infrastructure;
+
+import org.springframework.stereotype.Service;
+
+import com.ndhuy.proto_library.user.UserProto.AuthRequest;
+import com.ndhuy.proto_library.user.UserProto.AuthResponse;
+import com.ndhuy.proto_library.user.UserServiceGrpc;
+
+import net.devh.boot.grpc.client.inject.GrpcClient;
+
+@Service
+public class UserGrpc {
+    
+    @GrpcClient("user-service")
+    private UserServiceGrpc.UserServiceBlockingStub userServiceStub;
+    
+    public void authenticate(String username, String password) {
+        AuthRequest request = AuthRequest.newBuilder()
+                .setUsername(username)
+                .setPassword(password)
+                .build();
+
+        AuthResponse response = userServiceStub.authenticate(request);
+
+        System.out.println("Response: " + response);
+    }
+}
