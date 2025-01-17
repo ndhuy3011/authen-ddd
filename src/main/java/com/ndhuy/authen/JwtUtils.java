83120f7e62d76191174b09147e4ac42e777bc558
duchuy <ndhuy30111@gmail.com>
2025-01-17 15:46:24 +0700

    Refactor JWT utility to use JwtEncoder for token generation and update UsernameSubClaimAdapter structure

--- src/main/java/com/ndhuy/authen/JwtUtils.java
+++ src/main/java/com/ndhuy/authen/JwtUtils.java
@@ -1,13 +1,30 @@
 package com.ndhuy.authen;
 
+import java.util.UUID;
+
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+
+import jakarta.annotation.Resource;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
 public class JwtUtils {
 
-
-
-    public static String generatorJWT(){
-
-
-        return "";
-    }
+    @Resource
+    private JwtEncoder jwtEncoder;
+    public static String generatorJWT( UUID uuid) {
     
+
+        var claim = JwtClaimsSet.builder()
+        .id(uuid.toString())
+        .expiresAt(null)
+        .issuedAt(null)
+        .build();
+
+        var param = JwtEncoderParameters.from( claim);
+        return jwtEncoder.encode(param).getTokenValue();
+    }
+
 }
--- src/main/java/com/ndhuy/authen/UsernameSubClaimAdapter.java
+++ src/main/java/com/ndhuy/authen/UsernameSubClaimAdapter.java
@@ -6,14 +6,17 @@ import java.util.Map;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.oauth2.jwt.MappedJwtClaimSetConverter;
 
-public class UsernameSubClaimAdapter  implements Converter<Map<String, Object>, Map<String, Object>> {
-    
- private final MappedJwtClaimSetConverter delegate =
-            MappedJwtClaimSetConverter.withDefaults(Collections.emptyMap());
+public class UsernameSubClaimAdapter implements Converter<Map<String, Object>, Map<String, Object>> {
+
+    private final MappedJwtClaimSetConverter delegate = MappedJwtClaimSetConverter.withDefaults(Collections.emptyMap());
+
     @Override
     public Map<String, Object> convert(Map<String, Object> arg0) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'convert'");
     }
+
+
     
+
 }
